648dceb8f7337e3ba44b47ba7833c57aed686ef2
Massiah <jormas@amazon.com>
2018-07-31 17:52:32 +0100

    Name correction

--- test/test_file.py
+++ test/test_file.py
@@ -1,11 +1,11 @@
 from unittest import TestCase
 
 import hello_world
-import tested
+import somewhattested
 
 class MyTestCase(TestCase):
   def test_pass(self):
     return
 
   def test_qwerty(self):
-    assert tested.qwerty() == 6
+    assert somewhattested.qwerty() == 6
