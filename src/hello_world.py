52fb76258e32cf7249742f9b62d598b17ae32e7a
Jordan <jmas@live.co.uk>
2018-08-06 13:40:41 +0100

    Update hello_world.py

--- src/hello_world.py
+++ src/hello_world.py
@@ -1,11 +1,15 @@
 import numpy as np
 
+
 def hello_world():
-  print('hello world')
+    print('hello world')
+
 
 def second_function():
-  x = 1 + 2
-  print(x)
+    np.array([1, 2, 3])
+    x = 1 + 2
+    print(x)
+
 
 def say_hi(name):
-  return 'hi ' + name
+    return 'hi ' + name
