f7010ca895fdd227f696945f9d4b024678d40460
Jordan <jmas@live.co.uk>
2018-08-29 11:03:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,8 +4,12 @@ if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew update
   brew outdated openssl || brew upgrade openssl
   brew outdated readline || brew upgrade readline
-  brew install python$PYTHON
-  virtualenv venv -p python$PYTHON
+  
+  brew outdated readline || brew upgrade readline
+  pyenv install $PYTHON
+  export PYENV_VERSION=$PYTHON
+  export PATH="/Users/travis/.pyenv/shims:${PATH}"
+  virtualenv venv -p python3
   source venv/bin/activate
   python --version
 ]
